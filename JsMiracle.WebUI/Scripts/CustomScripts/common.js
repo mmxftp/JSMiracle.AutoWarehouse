63e46b1e53b236933ea86a38a3dd7ea1bc58daf9
mmx <mmx@MMXFTP>
2015-11-02 15:33:44 +0800

    Edit UserInfo (Add File)

---
+++ JsMiracle.WebUI/Scripts/CustomScripts/common.js
@@ -0,0 +1,42 @@
+﻿
+// 用于easyui datagrid 得到后台传送的日期格式化方法
+function dateTimeFormatter(value, row, index) {
+    if (value == null)
+        return '';
+
+    var obj = eval('(' + " new " + value.substr(1, value.length - 2) + ')');
+    return moment(obj).format('YYYY-MM-DD HH:mm:ss')
+}
+
+
+// 弹出浮层
+
+// 页面初始化时加入新的用于显示浮动页面的层。
+$(function () {
+    $('body').append('<div id="floatingLayerWindow" closed="true"></div>')
+})
+
+// 加入需显示的内容，并打开此层
+function showWindow(title, href, width, height, modal, minimizable, maximizable) {
+    $('#floatingLayerWindow').window(
+    {
+        title: title,
+        //href: href,
+        width: width,
+        height: height,
+        iconCls: 'icon-edit',
+
+        content: '<iframe scrolling="yes" frameborder="0"  src="'
+                + href
+                + '" style="width:90%;height:90%;" ></iframe>',
+
+        modal: modal === undefined ? true : modal,
+        minimizable: minimizable === undefined ? false
+                : minimizable,
+        maximizable: maximizable === undefined ? false
+                : maximizable,
+        closed: true,
+        loadingMessage: '正在加载数据，请稍等片刻......'
+    });
+    $('#floatingLayerWindow').window('open');
+}
